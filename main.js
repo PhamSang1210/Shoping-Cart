004471e62d06ac5594888e4a612d6c51487dbb6b
Pham Sang <phamhongsang12x10@gmail.com>
2023-07-11 09:04:26 +0700

    LocalStorage and fix decrement < 0

--- main.js
+++ main.js
@@ -31,12 +31,13 @@ let shopItemsData = [
     },
 ];
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("DATA")) || [];
 
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData
         .map((items) => {
             let { id, name, price, desc, img } = items;
+            let search = basket.find((x) => x.id === id) || [];
             return `
     <div id="product-id-${id}" class="item">
         <figure class="thumb">
@@ -56,7 +57,9 @@ let generateShop = () => {
                         style="--color: red"
                         class="bi bi-dash-lg change"
                     ></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">
+                       ${search.item === undefined ? 0 : search.item}
+                    </div>
                     <i
                         onclick="increment(${id})"
                         style="--color: green"
@@ -85,6 +88,7 @@ let increment = (id) => {
     } else {
         search.item++;
     }
+    localStorage.setItem("DATA", JSON.stringify(basket));
     update(seclectItem.id);
 };
 
@@ -93,12 +97,17 @@ let decrement = (id) => {
     let search = basket.find((element) => {
         return element.id === seclectItem.id;
     });
+    //Check click equal 0
+    if (search === undefined) return;
+    //End
     if (search.item === 0) {
         return;
     } else {
         search.item--;
     }
     update(seclectItem.id);
+    basket = basket.filter((x) => x.item !== 0);
+    localStorage.setItem("DATA", JSON.stringify(basket));
 };
 
 let update = (id) => {
@@ -119,3 +128,16 @@ let caculator = () => {
             return result + item;
         }, 0);
 };
+
+caculator();
+
+// SessionStorage
+// let input = document.querySelector(".temp");
+
+// input.value = sessionStorage.getItem("draft");
+
+// input.addEventLis    tener("change", asdasidha);
+
+// function asdasidha() {
+//     sessionStorage.setItem("draft", input.value);
+// }
